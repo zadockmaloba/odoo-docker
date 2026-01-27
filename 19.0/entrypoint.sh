5a5a4501122b3eae8d18c6799c537f99f6e7859e
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2026-01-28 02:15:17 +0300

    Rename DB env vars to avoid conflict with DigitalOcean env vars

--- 19.0/entrypoint.sh
+++ 19.0/entrypoint.sh
@@ -8,10 +8,10 @@ fi
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${ODOO_DB_HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${ODOO_DB_PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${ODOO_DB_USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${ODOO_DB_PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
 DB_ARGS=()
 function check_config() {
@@ -23,10 +23,10 @@ function check_config() {
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
+check_config "db_host" "$ODOO_DB_HOST"
+check_config "db_port" "$ODOO_DB_PORT"
+check_config "db_user" "$ODOO_DB_USER"
+check_config "db_password" "$ODOO_DB_PASSWORD"
 
 case "$1" in
     -- | odoo)
